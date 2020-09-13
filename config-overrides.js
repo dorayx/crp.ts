3f0603f3d283807634de1d01c1aca758ac860775
Doray Hong <hongduhui@gmail.com>
2020-09-13 18:30:32 +0800

    chore: upgrade ts and add cra rewired

---
+++ config-overrides.js
@@ -0,0 +1,29 @@
+const {
+  override,
+  addDecoratorsLegacy,
+  disableEsLint,
+  addBundleVisualizer,
+  addWebpackAlias,
+  adjustWorkbox,
+} = require('customize-cra');
+
+module.exports = override(
+    // enable legacy decorators babel plugin
+    addDecoratorsLegacy(),
+
+    // disable eslint in webpack
+    disableEsLint(),
+
+    // add webpack bundle visualizer if BUNDLE_VISUALIZE flag is enabled
+    process.env.BUNDLE_VISUALIZE == 1 && addBundleVisualizer(),
+
+    addWebpackAlias({}),
+
+    // adjust the underlying workbox
+    adjustWorkbox(wb =>
+        Object.assign(wb, {
+          skipWaiting: true,
+          exclude: (wb.exclude || []).concat('index.html'),
+        }),
+    ),
+);
